7a1a165e69f50189e35bab187f72ce76bc236118
tomasz.bednarski <tomasz.bednarski@ardigen.com>
2017-05-23 11:18:33 +0200

    adding additional code for signal filtering

--- signalFreqFiltering.R
+++ signalFreqFiltering.R
@@ -2,10 +2,10 @@
 
 library(signal)
 
-acq.freq <- 200
+acq.freq <- 200  # data acquisition frequency (Hz)    # for given freq, Nyquist freq = 100 Hz
 time     <- 1
 w        <- 2*pi/time
-ts       <- seq(0,time,1/acq.freq)
+ts       <- seq(0,time,1/acq.freq)   # vector of sampling time-points (s) 
 
 signal <- 3*sin(3*w*ts) 
 
@@ -16,10 +16,11 @@ noise2 <- 1.1 * sin(7*w*ts)
 noisedSig <- signal + noise
 noisedSig <- signal + noise + noise2
 
+head(signal)
 
-plot(signal, type = "l")
-plot(noise, type = "l")
-plot(noisedSig, type="l")
+plot(ts, signal, type = "l")
+plot(ts, noise, type = "l")
+plot(ts, noisedSig, type="l")
 
 #ftt()
 
@@ -48,6 +49,11 @@ plot(f_data_noisedSig$freq[harmonics]*length(noisedSig),
 ?butter     # typ filtra
 ?filtfilt    # proces filtracji
 
+#https://en.wikipedia.org/wiki/Butterworth_filter
+
+# W = 1/10 - mean 1/10th of Nyquist freq
+# Nyquist freq = 0.5 of sampling freq; sampling freq = 1/ sample step
+
 bf <- butter(n = 2, W = 1/10, type = "low" ) 
 b1 <- filtfilt(bf, noisedSig)
 
@@ -58,7 +64,59 @@ b2 <- filtfilt(bf2, noisedSig)
 
 freqz(bf2)
 
-plot(ts, b1, type = "l")
-lines(ts, b2, type = "l", col = "red")
 
+bf3 <- butter(n = 2, W = 1, type = "low" ) 
+b3 <- filtfilt(bf3, noisedSig)
+freqz(bf3)
+
+
+bf4 <- butter(n = 2, W = 1/15, type = "low" ) 
+b4 <- filtfilt(bf4, noisedSig)
+freqz(bf4)
+
+
+plot(x = ts, y = noisedSig, cex =  .5)
+
+#plot(ts, b1, type = "l")
+lines(ts, b1, type = "l", col = "green")
+lines(ts, b2, type = "l", col = "red")
+lines(ts, b3, type = "l", col = "yellow")
+lines(ts, b4, type = "l", col = "blue")
+
+
+bf5 <- butter(n = 2, W = 1/20, type = "low" ) 
+b5 <- filtfilt(bf5, noisedSig)
+freqz(bf5)
+
+lines(ts, b5, type = "l", col = "pink")
+
+# W = 1/20 juz zaczyna splaszczac sygnaly
+
+
+head(b4)
+
+b_temp<-b2
+
+f_data_temp <- GeneCycle::periodogram(b_temp)
+plot(f_data_temp$freq[harmonics]*length(b_temp), 
+     f_data_temp$spec[harmonics]/sum(f_data_temp$spec), 
+     xlab="Harmonics (Hz)", ylab="Amplitute Density", type="h", main = "1/5")
+
+
+
+#################
+
+
+bfH <- butter(n = 2, W = 1/10, type = "high" ) 
+bh1 <- filtfilt(bfH, noisedSig)
+
+plot(x = ts, y = noisedSig, cex =  .5)
+lines(ts, bh1, type = "l", col = "green")
+
+b_temp<-bh1
+
+f_data_temp <- GeneCycle::periodogram(b_temp)
+plot(f_data_temp$freq[harmonics]*length(b_temp), 
+     f_data_temp$spec[harmonics]/sum(f_data_temp$spec), 
+     xlab="Harmonics (Hz)", ylab="Amplitute Density", type="h", main = "h 1/10")
 
